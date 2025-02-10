6c35faa400cf892f9563dc74d68ccc5cf2a4903c
Tejas-Keskar <keskartejas01@gmail.com>
2025-02-10 13:15:04 +0530

    .

--- project_ui/main.py
+++ project_ui/main.py
@@ -4,26 +4,107 @@ from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 
-departments_data = {
-    "BFSI, Investments & Trading": ["Lending", "Banking Operations", "Life Insurance", "Others", "General Insurance"],
-    "Consulting": ["IT Consulting", "Management Consulting", "Others"],
-    "Administration & Facilities": ["Administration", "Others"],
-    "Data Science & Analytics": ["Data Science & Machine Learning", "Business Intelligence & Analytics", "Others"],
-    "Finance & Accounting": ["Accounting & Taxation", "Finance", "Others"],
-    "Healthcare & Life Sciences": ["Doctor", "Nursing", "Others"],
-    "Human Resources": ["HR Operations", "Recruitment & Talent Acquisition", "Others"],
-    "Marketing & Communication": ["Marketing", "Digital Marketing", "Others"],
-    "Production, Manufacturing & Engineering": ["Engineering", "Operations", "Others"],
-    "Sales & Business Development": ["Sales Support & Operations", "Enterprise & B2B Sales", "Others"],
-    "Customer Success, Service & Operations": ["Customer Success", "Operations", "Others"],
-    "Engineering - Software & QA": ["Software Development", "DevOps", "Testing", "Others"],
-    "Project & Program Management": ["IT Project Management", "Finance", "Others"],
-    "Research & Development": ["Engineering & Manufacturing", "Others"],
-    "Food, Beverage & Hospitality": ["Food Services", "Hospitality", "Others"]
+
+
+
+cities_list = ['Ahmedabad', 'Bengaluru', 'Chandigarh', 'Chennai', 'Coimbatore',
+    'Delhi', 'Faridabad', 'Gurugram', 'Hyderabad', 'Jaipur', 'Kochi',
+    'Kolkata', 'Lucknow', 'Madurai', 'Mohali', 'Mumbai', 'Noida',
+    'Pune', 'Surat', 'Vadodara', 'other'
+]
+    # Define experience ranges with their corresponding labels
+departments_data =  {'Administration & Facilities': ['Administration', 'Facility Management'],
+ 'BFSI, Investments & Trading':['General Insurance',
+   'Banking Operations',
+   'Others',
+   'Lending',
+   'Life Insurance'],
+'Consulting':['IT Consulting',
+   'Other Consulting',
+   'Management Consulting'],
+'Content, Editorial & Journalism':['Content Management (Print / Online / Electronic)',
+   'Content, Editorial & Journalism - Others'],
+ 'Customer Success, Service & Operations'
+ :['Customer Success',
+   'Operations Support',
+   'Others',
+   'Operations',
+   'Service Delivery',
+   'After Sales Service & Repair',
+   'Customer Success, Service & Operations - Other',
+   'Voice / Blended',
+   'Back Office'],
+ 'Data Science & Analytics' :['Business Intelligence & Analytics',
+   'Data Science & Analytics - Other',
+   'Data Science & Machine Learning'],
+ 'Engineering - Software & QA':['Others',
+   'Quality Assurance and Testing',
+   'DBA / Data warehousing',
+   'DevOps',
+   'Software Development'],
+'Environment Health & Safety': ['Others',
+   'Environment Health and Safety - Other',
+   'Occupational Health & Safety'],
+ 'Finance & Accounting' : ['Finance & Accounting - Others',
+   'Finance & Accounting - Other',
+   'Finance',
+   'Treasury',
+   'Accounting & Taxation',
+   'Audit & Control'],
+ 'Food, Beverage & Hospitality':['F&B Service',
+   'Kitchen / F&B Production',
+   'Food, Beverage & Hospitality - Others',
+   'Front Office & Guest Services'],
+ 'Healthcare & Life Sciences': ['Doctor',
+   'Imaging & Diagnostics',
+   'Health Informatics',
+   'Healthcare & Life Sciences - Other',
+   'Nursing',
+   'Other Hospital Staff'],
+ 'Human Resources': ['HR Operations',
+   'Others',
+   'Employee Relations',
+   'Recruitment & Talent Acquisition',
+   'Compensation & Benefits',
+   'Human Resources - Other'],
+ 'Legal & Regulatory': ['Legal Operations',
+   'Others',
+   'Legal & Regulatory - Other',
+   'Corporate Affairs'],
+ 'Marketing & Communication':['Digital Marketing',
+   'Marketing',
+   'Corporate Communication',
+   'Marketing & Communication - Others',
+   'Advertising & Creative'],
+ 'Merchandising, Retail & eCommerce': ['Retail Store Operations',
+   'Merchandising, Retail & eCommerce - Others'],
+ 'Others':['Others - Others'],
+ 'Product Management': ['Product Management - Other',
+   'Product Management - Technology'],
+ 'Production, Manufacturing & Engineering': ['Management',
+   'Operations, Maintenance & Support',
+   'Production & Manufacturing - Other',
+   'Engineering'],
+ 'Project & Program Management':['Other Program / Project Management',
+   'Technology / IT',
+   'Finance',
+   'Construction / Manufacturing'],
+'Research & Development':['Others',
+   'Research & Development - Other',
+   'Pharmaceutical & Biotechnology',
+   'Engineering & Manufacturing'],
+ 'Risk Management & Compliance': ['Risk Management & Compliance - Others',
+   'Finance',
+   'Risk Management & Compliance - Other'],
+ 'Sales & Business Development': ['Sales Support & Operations',
+   'Others',
+   'BD / Pre Sales',
+   'Enterprise & B2B Sales'],
+ 'Sports, Fitness & Personal Care':['Others', 'Health & Fitness', 'Beauty & Personal Care'],
+'Strategic & Top Management': ['Strategic & Top Management - Others',
+   'Top Management']
 }
 
-    
-    # Define experience ranges with their corresponding labels
 experience_dict = {
     "Entry-level": (0, 1),
     "Junior": (1, 3),
@@ -33,21 +114,44 @@ experience_dict = {
     "Expert": (15, float('inf'))  # Using infinity for experience above 15
 }
 
-# ['Entry-level', 'Experienced', 'Expert', 'Junior', 'Mid-level',
-#        'Senior']
+sal_dict  = {
+    0: (0, 350000),
+    1: (1000000, 1500000),
+    2: (350000, 600000),
+    3: (600000, 1000000),
+    4: (1500000, float('inf'))  # Use float('inf') for 'above 1500000'
+}
+
+
+
+with open("salary_model.pkl", "rb") as file:
+    model = pickle.load(file)
+
+
 
 with open("Department_encoder.pkl", "rb") as file:
     department_encoder = pickle.load(file)
 
+with open("role_category_encoder.pkl", "rb") as file:
+    role_category_encoder = pickle.load(file)
+
+
+
 with open("experience_Category_encoder.pkl", "rb") as file:
     experience_Category_encoder = pickle.load(file)
+
+with open("city_encoder.pkl", "rb") as file:
+    city_encoder = pickle.load(file)
+
+with open("salencoder.pkl", "rb") as file:
+    salencoder = pickle.load(file)
 
 
 @app.route('/', methods=["GET"])
 def root():
     # Pass departments data keys to the template
     departments = list(departments_data.keys())
-    return render_template('index.html', departments=departments)
+    return render_template('index.html', departments=departments, cities_list=cities_list)
 
 @app.route('/get_roles', methods=["POST"])
 def get_roles():
@@ -57,44 +161,36 @@ def get_roles():
     return jsonify({"roles": role_categories})
 
 @app.route('/predict', methods=["POST"])
+
 def predict_churn_value():
-    # print(request.form)
-    # department = request.form.get("department_encoder")
     department = department_encoder.transform([request.form.get('department')])
-
-    role_category = request.form.get("role_category")
-
-    # experience = request.form.get("experience")
+    city = city_encoder.transform([request.form.get('city')])
+    role_category = role_category_encoder.transform([request.form.get("role_category")])
     experience = int(request.form.get("experience"))
 
-
     # Find the corresponding experience level
-    experience_level = None
-    for level, (min_exp, max_exp) in experience_dict.items():
-        if min_exp <= experience <= max_exp:
-            experience_level = level
-            break
-
-    # If no match is found (just a safety check)
-    if experience_level is None:
-        experience_level = "Unknown"
-
-
+    experience_level = next((level for level, (min_exp, max_exp) in experience_dict.items() if min_exp <= experience <= max_exp), "Unknown")
     experience = experience_Category_encoder.transform([experience_level])
 
-
-    # Print the experience level to console for debugging
-    print("Experience Level:", experience_level)
-    print("exp level value",experience[0])
     education_ug = int(request.form.get("education_ug"))
     education_pg = int(request.form.get("education_pg"))
-    print(department[0])
-    print(role_category)
-    print(experience)
-    print(education_ug)
-    print(education_pg)
-    return "model"
 
+    # Model input and prediction
+    result = [experience[0], city[0], education_ug, education_pg, department[0], role_category[0]]
+    predicted_code = model.predict([result])[0]
+
+    # Find salary range
+    salary_range = next(
+        (f"{min_exp} to {max_exp}" for salcode, (min_exp, max_exp) in sal_dict.items() if salcode == predicted_code),
+        None
+    )
+
+    # If no valid prediction is found
+    if not salary_range:
+        return render_template("error.html", message="Something went wrong during prediction.")
+
+    # Render the result page
+    return render_template("result.html", salary_range=salary_range)
 
 # @app.route('/predict', methods=["POST"])
 # def predict_churn_value():
